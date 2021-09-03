b59c4e77d58dcc33fe6fb8fe2ff6fd332451cd61
Teo Si-Yan <teosiyan@hotmail.com>
2021-09-04 00:18:48 +0800

    Update brew script and packages (add alfred, karabiner-elements)

--- brew.sh
+++ brew.sh
@@ -3,21 +3,23 @@
 # Install Homebrew or make sure it's up to date
 which -s brew
 if [[ $? != 0 ]] ; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo "Installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
+  echo "Updating homebrew"
   brew update
   brew upgrade
 fi
 
-# Install brew and cask packages from Brewfile
+echo "Installing brew and cask packages from Brewfile"
 brew bundle
 
-# Switch default shell to brew-installed one
+echo "Switching default shell to brew-installed one"
 BREW_PREFIX=$(brew --prefix)
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-# Cleanup
+echo "Running brew cleanup"
 brew cleanup
