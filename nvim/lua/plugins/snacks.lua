d0e0a63fec01e7b5be3d68232ac647b729d96d1b
Si-Yan Teo <siyan@allium.so>
2026-01-15 16:16:30 +0800

    nvim snacks show file path as preview title

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,6 +3,12 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
+        -- Transform to show full relative file path in preview title
+        transform = function(item)
+          if item.file then
+            item.preview_title = item.file
+          end
+        end,
         sources = {
           files = {
             hidden = true,
